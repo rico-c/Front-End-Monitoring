969cabdc62d2f106eac3c8308830cbe668ac9132
caoyu <ricardocao.biker@gmail.com>
2018-12-08 17:43:48 +0800

    finish NewErr

--- MonitorPlatformCode/monitorplatform/src/page/errInfo/newErr.js
+++ MonitorPlatformCode/monitorplatform/src/page/errInfo/newErr.js
@@ -6,30 +6,40 @@ import { getNewErr } from '../../API/api';
 import '../css/newErr.css';
 
 class NewErr extends React.Component {
-    state = {
-        data: [],
-        loading: false,
-        hasMore: true,
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            loading: false,
+            hasMore: true,
+            startId: ''
+        }
+        this.handleInfiniteOnLoad = this.handleInfiniteOnLoad.bind(this)
     }
 
     componentDidMount() {
         Axios.get(getNewErr)
             .then((res) => {
-                console.log(res)
+                this.setNewStart(res.data[res.data.length - 1].id);
                 this.setState({
                     data: res.data
-                })
+                });
             })
     }
 
-    handleInfiniteOnLoad = () => {
+    handleInfiniteOnLoad() {
         let data = this.state.data;
         this.setState({
             loading: true,
         });
-        Axios.get(getNewErr)
+        Axios.get(getNewErr, {
+            params: {
+                startId: this.state.startId
+            }
+        })
             .then((res) => {
                 data = data.concat(res.data);
+                this.setNewStart(res.data[res.data.length - 1].id);
                 this.setState({
                     data,
                     loading: false,
@@ -37,7 +47,32 @@ class NewErr extends React.Component {
             })
     }
 
+    // 滚动加载
+    setNewStart(Id) {
+        this.setState({ startId: Number(Id) - 1 })
+    }
+
     render() {
+        function os(item) {
+            if (item.ua.indexOf('Android') > -1 || item.ua.indexOf('Adr') > -1) {
+                return <span> <Icon type="android" /> 安卓 </span>
+            } else if (!!item.ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+                return <span><Icon type="apple" />苹果</span>
+            } else {
+                return <span><Icon type="chrome" />浏览器 </span>
+            }
+        }
+        function type(item) {
+            if (item.type === 'jsRuntime') {
+                return <div className="typeIcon typeIcon1">JS</div>
+            } else if (item.type === 'sourceLoad') {
+                return <div className="typeIcon typeIcon2">Load</div>
+            } else if (item.type === 'apiRequest') {
+                return <div className="typeIcon typeIcon3">API</div>
+            } else {
+                return <div className="typeIcon typeIcon4">{item.type ? item.type : '其他'}</div>
+            }
+        }
         return (
             <div className="infinite-container">
                 <InfiniteScroll
@@ -52,22 +87,14 @@ class NewErr extends React.Component {
                         renderItem={item => (
                             <List.Item key={item.id}>
                                 <div>
-                                    <p>{item.type}</p>
+                                    {type(item)}
                                 </div>
-                                <div>
+                                <div className="itemInfo">
                                     <p><Icon type="code" /><span> 信息：{item.msg.substring(0, 350)}{item.msg.length >= 350 ? ' ......' : ''}</span></p>
                                     <p><Icon type="paper-clip" /><span> 来源：{item.errUrl.substring(0, 120)}{item.errUrl.length >= 120 ? ' ......' : ''}</span></p>
                                     <p className="subInfo">
                                         <span className="inline"><Icon type="slack" /> 设备：
-                                        {
-                                            if (item.ua.indexOf('Android') > -1 || item.ua.indexOf('Adr') > -1){
-                                                (<Icon type="android" />安卓)
-                                            }else if(!!item.ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)){
-                                                (<Icon type="apple" />苹果)
-                                            }else{
-                                                (<Icon type="chrome" />浏览器)
-                                        }
-                                    }
+                                        {os(item)}
                                         </span>
                                         <span><Icon type="user-add" /> 登录：{JSON.parse(item.loginInfo).status === 'login' ? '已登录' : '未登录'}</span>
                                         <span><Icon type="compass" /> 位置：</span>
